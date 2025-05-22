22b1578af3076993d6094086a6480601bbe27f2f
Paul Prabahar <paul.prabahar@aravind.org>
2025-05-22 14:42:30 +0530

    repository and interface for token generation, injection to controller pending.

---
+++ NZWalks/Repository/Interface/ICreateTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.Repository.Interface;
+
+public interface ICreateTokenRepository
+{
+    string CreateJWTToken(IdentityUser user, List<string> roles);
+}
---
+++ NZWalks/Repository/Repo/NsgpCreateJWTTokenRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using NZWalks.Repository.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NZWalks.Repository.Repo;
+
+public class NsgpCreateJWTTokenRepository : ICreateTokenRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public NsgpCreateJWTTokenRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string CreateJWTToken(IdentityUser user, List<string> roles)
+    {
+        //create claims
+        var claims = new List<Claim>();
+
+        claims.Add(new Claim(ClaimTypes.Email, user.Email)); 
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        //create key
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+        //create Credientials
+        var credientials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        //Create Token
+        var token = new JwtSecurityToken(
+            _configuration["Jwt:Issuer"],
+            _configuration["Jwt:Audience"],
+            claims,
+            expires:DateTime.UtcNow.AddMonths(14),
+            signingCredentials:credientials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
